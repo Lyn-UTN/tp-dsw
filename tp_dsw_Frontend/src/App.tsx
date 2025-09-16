ad51da52722f5a066d44888b61866452aeb3b046
Lucas Yaccuzzi <lucasyaccuzzi@gmail.com>
2025-09-16 14:26:49 -0300

    Avance con Frontend

--- tp_dsw_Frontend/src/App.tsx
+++ tp_dsw_Frontend/src/App.tsx
@@ -1,35 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Card, { CardBody } from './components/Card';
+import List from './components/List.tsx';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const list = ['Cocheras', 'Autos', 'Usuarios'];
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Card>
+      <CardBody title="AIRBNG" text="Mensaje random" />
+      <List data={list} />
+    </Card>
+  );
 }
 
-export default App
+export default App;
