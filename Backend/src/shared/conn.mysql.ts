be7025818e779963d16a91536f928df07a8084bb
Fausto Gabriele <faustoogabriele@outlook.com.ar>
2025-10-21 18:02:38 -0300

    agregue al git ignore el arch conn.mysql, fijense ustds de cambiar sus claves

--- Backend/src/shared/conn.mysql.ts
+++ Backend/src/shared/conn.mysql.ts
@@ -1,17 +1,15 @@
-import mysql from 'mysql2/promise';
+import mysql from "mysql2/promise";
 
 export const pool = mysql.createPool({
-
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER ||   'root',
-    password: process.env.DB_PASSWORD || 'PALANGANA2005',
-    database: process.env.DB_NAME || 'tp_dsw',
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
-    maxIdle: 10,
-    idleTimeout: 60000, // 60 seconds
-    enableKeepAlive: true, //evita timeout desde el lado del servidor
-    keepAliveInitialDelay: 0,
-
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "aldao338",
+  database: process.env.DB_NAME || "AIRBNG",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  maxIdle: 10,
+  idleTimeout: 60000, // 60 seconds
+  enableKeepAlive: true, //evita timeout desde el lado del servidor
+  keepAliveInitialDelay: 0,
 });
